e7cc863c8e9331da67595353b5162b2dd33a9e5d
Expired <imoses2@hotmail.com>
2022-02-28 22:50:00 -0700

    fixes #3

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,15 @@ values
 	('Quality Assurance'),
 	('Sales'),
 	('Production');
+	
+insert into roles (title, salary, department_id)
+values
+	('Front End Engineer', 60000, 1),
+	('Back End Engineer', 70000, 1),
+	('Accountant', 50000, 2),
+	('Quality Assessor', 40000, 3),
+	('Salesperson', 60000, 4),
+	('Marketer', 70000, 4),
+	('Manufacturer', 45000, 5),
+	('Shift Lead', 50000, 5),
+	('Maintenance', 45000, 5);
