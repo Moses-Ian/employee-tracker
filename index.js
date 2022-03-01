7426912a10ea79718a81313beca66f8225373d74
Expired <imoses2@hotmail.com>
2022-02-28 19:55:38 -0700

    accepting an answer

--- index.js
+++ index.js
@@ -18,4 +18,22 @@ const promptUser = () => {
 	}]);
 }
 
-promptUser();
+promptUser()	// returns a promise
+	.then(answers => {
+		let { action } = answers;
+		action = action.trim().toLowerCase().split(' ');
+		console.log(action);
+		switch(action[0]) {
+			case 'view':
+				//do something
+				break;
+			case 'add':
+				//do something
+				break;
+			case 'update':
+				//do something
+				break;
+			default:
+				//do something
+		}
+	});
